9e6c2fb8385b971d582e13ab3c160a3ddd17c5ad
nranderson14 <NRAnderson14@winona.edu>
2016-09-22 20:10:18 -0500

    Added jQuery

--- scripts.js
+++ scripts.js
@@ -1,9 +1,39 @@
-var main = function() {
-	
-}
-
-$(document).ready(main);
-
 function buttonFunction() {
 	alert("You pressed the button");
 }
+
+$(document).ready(function(){
+	$(".btn").click(function(){
+		$("h1").toggle(1000);
+	});
+	
+	$("#btntop").click(function(){
+		$("#top").toggle(750);
+	});
+	
+	$("#btnmid").click(function(){
+		$("#mid").toggle(750);
+	});
+	
+	$("#btnbtm").click(function(){
+		$("#btm").toggle(750);
+	});
+	
+	$("#top").hover(function(){
+		$("#top").fadeTo(100, 1);
+	}, function(){
+		$("#top").fadeTo(100, .25);
+	});
+	
+	$("#mid").hover(function(){
+		$("#mid").fadeTo(100, 1);
+	}, function(){
+		$("#mid").fadeTo(100, .25);
+	});
+	
+	$("#btm").hover(function(){
+		$("#btm").fadeTo(100, 1);
+	}, function(){
+		$("#btm").fadeTo(100, .25);
+	});
+});
