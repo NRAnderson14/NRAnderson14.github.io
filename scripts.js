adb278d032c3599f5fc7cb9b6a7bd9ccd9607163
nranderson14 <NRAnderson14@winona.edu>
2016-09-29 21:39:31 -0500

    Updated JS

--- scripts.js
+++ scripts.js
@@ -1,45 +1,79 @@
-function print(e, id) {
-	document.getElementById(id).innerHTML = e;
-}
-
-
-function arrayStringBuilder(a) {
-	var returnString = "";
-
-	for (var i = 0; i < a.length; i++) {
-		returnString += a[i] + " ";
-	}
-
-	return returnString;
-}
-
-
+/*
+ *	Takes an array a and sorts it in ascending order
+ */
 function bubbleSort(a) {
-	var pass   = 1;
+	var swapped = true;
+	var j = 0;
 
-	do {
-		var swapped = false;
-		for (var i = 0; i < a.length - pass; i++) {
+	while (swapped) {
+		swapped = false;
+		j++;
+
+		for (var i = 0; i < a.length; i++) {
 			if (a[i] > a[i+1]) {
 				var temp = a[i];
-				a[i]     = a[i+1];
-				a[i+1]   = temp;
+				a[i] 		 = a[i+1];
+				a[i+1]	 = temp;
 				swapped  = true;
 			}
-			pass++;
 		}
-	} while (swapped);
+	}
 }
 
-
+/*
+ *	Creates an array of random integers from 0 - 99 of size
+ *		specified by input n
+ */
 function generateArray(n) {
 	var a = [-1];
 
-	for (var i = 0; i <= a.length; i++) {
+	for (var i = 0; i <= n; i++) {
 		a[i] = parseInt(Math.random() * 100);
 	}
 
 	return a;
 }
 
-var nums = [1, 2, 3, 4, 5];
+/*
+ *	Generates a sorted array of length specified by input n
+ */
+function genSortArray(n) {
+	var arr = generateArray(n);
+	bubbleSort(arr);
+	return arr;
+}
+
+/*
+ *	Takes an array a, and creates divs in the container id
+ *		with the corresponding array values
+ */
+function parseArray(a) {
+	for (var i = 0; i < a.length; i++) {
+		var bdiv 		 	 = document.createElement("div");
+		bdiv.className = "arrayElement";
+		bdiv.idName		 = a[i];
+		var bdivText 	 = document.createTextNode(a[i]);
+		bdiv.appendChild(bdivText);
+		document.getElementById("container").appendChild(bdiv);
+	}
+}
+
+/*
+ *	Deletes any elements in the DOM with the class arrayElement
+ */
+function deletePriorArray() {
+	var elements = document.getElementsByClassName("arrayElement");
+	while(elements.length > 0) {
+		elements[0].parentNode.removeChild(elements[0]);
+	}
+}
+
+/*
+ *	Appends page with divs of an array
+ */
+function arrayDivMaker() {
+	deletePriorArray();
+	var arSize = document.getElementById("arraySize").value;
+	var arr 	 = genSortArray(arSize);
+	parseArray(arr);
+}
